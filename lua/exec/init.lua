723b3be71ff08a9230554433a551005fb8219267
aikhe <ikeandrie.ro@gmail.com>
2025-12-17 08:13:10 +0800

    chore: stylua

--- lua/exec/init.lua
+++ lua/exec/init.lua
@@ -1,21 +1,15 @@
 local M = {}
 
-local state = require("exec.state")
+local state = require "exec.state"
 
 M.setup = function(opts)
-	state.config = vim.tbl_deep_extend("force", state.config, opts or {})
+  state.config = vim.tbl_deep_extend("force", state.config, opts or {})
 
-	if state.mapping then
-		require("exec.mappings")()
-	end
+  if state.mapping then require "exec.mappings"() end
 end
 
-M.open = function()
-	print("open exec")
-end
+M.open = function() print "open exec" end
 
-M.toggle = function()
-	print("toggle exec")
-end
+M.toggle = function() print "toggle exec" end
 
 return M
--- plugin/exec.lua
+++ plugin/exec.lua
@@ -1,7 +1,3 @@
--- local call = require('Comment.api').call
-
-vim.api.nvim_create_user_command("Exec", function()
-	require("exec").toggle()
-end, {})
+vim.api.nvim_create_user_command("Exec", function() require("exec").toggle() end, {})
 
 require("exec").setup()
